778a3fc38c1d6a6084ea4958c026baff93b16f6d
Jared Yeo <jaredyeo@gmail.com>
2025-06-20 16:53:46 +0800

    .

--- src/routes/governance/roles-and-responsibilities/+page.svelte
+++ src/routes/governance/roles-and-responsibilities/+page.svelte
@@ -13,7 +13,7 @@
                 <li><a href="/">🏠Home</a></li>
                 <li><a href="/governance">Governance</a></li>
                 <li class="is-active">
-                    <a href="./" aria-current="page">Roles & Responsibilities</a
+                    <a href="./" aria-current="page">R & Rs</a
                     >
                 </li>
             </ul>
