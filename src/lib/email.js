18b8ea3723022d89dfc150e9a6795ba7fb7397e6
Jared Yeo <jaredyeo@gmail.com>
2025-07-19 07:50:20 +0800

    .

--- src/lib/email.js
+++ src/lib/email.js
@@ -1,51 +1,50 @@
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
-import { env } from '$env/dynamic/private';
+import { env } from "$env/dynamic/private";
 import { BREADBREAKERS_EMAIL_FROM } from "$lib/strings";
 
 const REGION = "us-east-1";
 
 export const ses = new SESClient({
-    region: REGION,
-    credentials: {
-        accessKeyId: env.BB_AWS_ACCESS_KEY_ID,
-        secretAccessKey: env.BB_AWS_SECRET_ACCESS_KEY,
-    },
+	region: REGION,
+	credentials: {
+		accessKeyId: env.BB_AWS_ACCESS_KEY_ID,
+		secretAccessKey: env.BB_AWS_SECRET_ACCESS_KEY
+	}
 });
 
 export async function sendEmail({ to, subject, body, replyto, bcc, cc }) {
+	const htmlBody = body.replace(/\n/g, "<br>");
 
-    const htmlBody = body.replace(/\n/g, '<br>');
+	const subjectClean = subject;
 
-    const subjectClean = subject;
+	const params = {
+		Destination: {
+			ToAddresses: [to],
+			CcAddresses: cc ? [cc] : [],
+			BccAddresses: bcc ? [bcc] : []
+		},
+		Message: {
+			Body: {
+				Html: {
+					Charset: "UTF-8",
+					Data: htmlBody
+				}
+			},
+			Subject: {
+				Charset: "UTF-8",
+				Data: subjectClean
+			}
+		},
+		Source: BREADBREAKERS_EMAIL_FROM,
+		ReplyToAddresses: replyto ? [replyto] : []
+	};
 
-    const params = {
-        Destination: {
-            ToAddresses: [to],
-            CcAddresses: cc ? [cc] : [],
-            BccAddresses: bcc ? [bcc] : []
-        },
-        Message: {
-            Body: {
-                Html: {
-                    Charset: "UTF-8",
-                    Data: htmlBody,
-                },
-            },
-            Subject: {
-                Charset: "UTF-8",
-                Data: subjectClean,
-            },
-        },
-        Source: BREADBREAKERS_EMAIL_FROM,
-        ReplyToAddresses: replyto ? [replyto] : [],
-    };
+	const command = new SendEmailCommand(params);
 
-    const command = new SendEmailCommand(params);
-    //console.log("Sending email with params:", params);
-    try {
-      await ses.send(command);
-    } catch (error) {
-      console.error("Error sending email:", error);
-      throw error;
-    }
+	try {
+		await ses.send(command);
+	} catch (error) {
+		console.error("Error sending email:", error);
+		throw error;
+	}
 }
--- src/routes/governance/privacy-policy/+page.svelte
+++ src/routes/governance/privacy-policy/+page.svelte
@@ -1,92 +1,71 @@
 <script>
-    import BackToTop from "$lib/components/BackToTop.svelte";
+	import BackToTop from "$lib/components/BackToTop.svelte";
 </script>
 
 <svelte:head>
-    <title>Bread Breakers (SG) | Privacy Policy</title>
+	<title>Bread Breakers (SG) | Privacy Policy</title>
 </svelte:head>
 
 <section class="section">
-    <div class="container">
+	<div class="container">
+		<h2 class="subtitle is-4 has-text-weight-bold pt-4">Privacy Policy</h2>
 
-        <h2 class="subtitle is-4 has-text-weight-bold pt-4">Privacy Policy</h2>
+		<div class="content">
+			<h3 class="subtitle is-5 mt-5 has-text-weight-bold">Data Subject Rights</h3>
+			<p>
+				You may access, correct, or withdraw your personal data. Requests will be processed in line
+				with PDPA.
+			</p>
 
-        <div class="content">
-            <h3 class="subtitle is-5 mt-5 has-text-weight-bold">
-                Data Subject Rights
-            </h3>
-            <p>
-                You may access, correct, or withdraw your personal data. Please
-                contact our Data Protection Officer (DPO). Requests will be
-                processed in line with PDPA.
-            </p>
+			<h3 class="subtitle is-5 mt-5 has-text-weight-bold">Breach Notification</h3>
+			<p>
+				Any data breach, whether suspected or confirmed, will be immediately reported to the DPO. If
+				there is significant risk, we will <a
+					class="link"
+					href="https://www.pdpc.gov.sg/report-data-breach/self-assessment">notify the PDPC</a
+				> and affected individuals as required.
+			</p>
 
-            <h3 class="subtitle is-5 mt-5 has-text-weight-bold">
-                Breach Notification
-            </h3>
-            <p>
-                Any data breach, whether suspected or confirmed, will be
-                immediately reported to the DPO. If there is significant risk,
-                we will <a
-                    class="link"
-                    href="https://www.pdpc.gov.sg/report-data-breach/self-assessment"
-                    >notify the PDPC</a
-                > and affected individuals as required.
-            </p>
+			<h3 class="subtitle is-5 mt-5 has-text-weight-bold">Disclosure of Personal Data</h3>
+			<p>We do not disclose, sell or rent your data. Disclosure only occurs under:</p>
+			<ul>
+				<li>
+					<strong>Legal Obligations:</strong> Law enforcement or government mandates
+				</li>
+				<li>
+					<strong>Consent:</strong> With your explicit written consent
+				</li>
+			</ul>
+			<h3 class="subtitle is-5 mt-5 has-text-weight-bold">Cookies and Authentication</h3>
+			<p>
+				We use cookies solely for authentication purposes. These cookies are essential for enabling
+				secure login and maintaining session integrity. They do not track users for advertising or
+				analytics purposes.
+			</p>
+			<p>
+				When you log in, we retrieve and store your email address. This email is used only for
+				authentication and authorization. We do not use this information for marketing, profiling,
+				or any third-party sharing.
+			</p>
 
-            <h3 class="subtitle is-5 mt-5 has-text-weight-bold">
-                Disclosure of Personal Data
-            </h3>
-            <p>
-                We do not sell or rent your data. Disclosure only occurs under:
-            </p>
-            <ul>
-                <li>
-                    <strong>Legal Obligations:</strong> Law enforcement or government
-                    mandates
-                </li>
-                <li>
-                    <strong>Consent:</strong> With your explicit written consent
-                </li>
-            </ul>
-            <h3 class="subtitle is-5 mt-5 has-text-weight-bold">
-                Cookies and Authentication
-            </h3>
-            <p>
-                We use cookies solely for authentication purposes. These cookies
-                are essential for enabling secure login and maintaining session
-                integrity. They do not track users for advertising or analytics
-                purposes.
-            </p>
-            <p>
-                When you log in, we retrieve and store your email address. This
-                email is used only for operational purposes such as verifying
-                identity, managing access permissions, and communicating
-                organizational updates. We do not use this information for
-                marketing, profiling, or any third-party sharing.
-            </p>
-
-            <h3 class="subtitle is-5 mt-5 has-text-weight-bold">AI Usage</h3>
-            <p>
-                We use Google’s Gemini AI models solely to verify the accuracy
-                of user submissions and to detect any inclusion of personal
-                data. All data is processed in accordance with Google’s
-                enterprise-level privacy and data protection policies. No
-                information is shared with Google for training or improving
-                their AI models, and we do not store any personal data. All
-                inputs are handled confidentially and used only for these
-                limited, clearly defined purposes.
-            </p>
-        </div>
-        <nav class="breadcrumb has-arrow-separator is-centered pt-5" aria-label="breadcrumbs">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/governance">Governance</a></li>
-                <li class="is-active">
-                    <a href="./" aria-current="page">Privacy Policy</a>
-                </li>
-            </ul>
-        </nav>
-        <BackToTop />
-    </div>
+			<h3 class="subtitle is-5 mt-5 has-text-weight-bold">AI Usage</h3>
+			<p>
+				We use Google’s Gemini AI models solely to verify the accuracy of user submissions and to
+				detect any inclusion of personal data. All data is processed in accordance with Google’s
+				enterprise-level privacy and data protection policies. No information is shared with Google
+				for training or improving their AI models, and we do not store any personal data. All inputs
+				are handled confidentially and used only for these limited, clearly defined purposes.
+			</p>
+		</div>
+		<nav class="breadcrumb has-arrow-separator is-centered pt-5" aria-label="breadcrumbs">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/governance">Governance</a></li>
+				<li class="is-active">
+					<a href="./" aria-current="page">Privacy Policy</a>
+				</li>
+			</ul>
+		</nav>
+		<BackToTop />
+	</div>
 </section>
