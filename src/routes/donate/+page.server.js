21a30498676c81d36f7eb65efc1767b67897ce2a
Jared Yeo <jaredyeo@gmail.com>
2025-08-12 10:27:48 +0800

    .

--- src/routes/donate/+page.server.js
+++ src/routes/donate/+page.server.js
@@ -3,7 +3,7 @@ export async function load({ locals }) {
         email: null
     });
 
-    const targetDonation = (data.inNeedCount + data.householdsWaiting) * 100;
+    const targetDonation = (data.inNeedCount) * 50;
     const operatingFund =(data.operatingIncoming / 100).toFixed(2);
     const beneFund = ((data.balanceData - data.ringfenceN - data.operatingIncoming) / 100).toFixed(2);
 
