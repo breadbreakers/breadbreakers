180ddd7bc206ad86fb78c4c3f4d0d343ccc4a51c
Jared Yeo <jaredyeo@gmail.com>
2025-08-12 10:31:16 +0800

    .

--- src/lib/strings.js
+++ src/lib/strings.js
@@ -1,5 +1,5 @@
 export const BREADBREAKERS_EMAIL_FROM = "Bread Breakers (SG) <hello@breadbreakers.sg>"
 export const BREADBREAKERS_EMAIL = "hello@breadbreakers.sg"
 export const OFFER_SUBJECT = 'Donation and delivery for';
-export const OFFER_EMAIL = 'Hello,%0D%0A%0D%0AMay I help purchase and deliver this item for your client?%0D%0A%0D%0AIf it\'s ok, could you share the recipient\'s address to my WhatsApp? (I have enabled disappearing messages) Name and mobile number is not needed as I will use mine. Also, is it okay if I could send a note of care and encouragement for your client?%0D%0A%0D%0AFeel free to share any preferences or additional details of the requested item. I\'ll try my best to accommodate.%0D%0A%0D%0AWarmest regards,';
-export const OFFER_WA = 'Hello, May I help purchase and deliver this item for your client? Do also let me know if I could convey a note of care and encouragement for your client. Feel free to share any preferences or additional details of the requested item. I\'ll try my best to accommodate. Thanks!';
+export const OFFER_EMAIL = 'Hello, is this item already fulfiled? Let me know. Thanks!%0D%0A%0D%0AWarmest regards,';
+export const OFFER_WA = 'Hello, is this item already fulfiled? Let me know. Thanks!';
